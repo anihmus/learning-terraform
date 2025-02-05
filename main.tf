0e414c1ccb47dfb0eaca2105b73541cf2733e467
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-05 08:13:22 -0700

    Rearrange blog vpc

--- main.tf
+++ main.tf
@@ -34,6 +34,22 @@ module "autoscaling" {
   instance_type          = var.instance_type
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
@@ -64,22 +80,6 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
-  }
-}
-
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
     Environment = "dev"
   }
 }
