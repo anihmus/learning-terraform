81c71d4f6d6f168a50eece7af17485c4092c7477
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-05 07:41:53 -0700

    Load balancer

--- main.tf
+++ main.tf
@@ -47,29 +47,45 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = "module.blog_vpc.vpc_id"
-  subnets = "module.blog_vpc.public_subnets"
+  name = "blog-alb"
 
-  security_groups = "module.blog_sg.security_group_id"
+  load_balancer_type = "application"
 
-  http_tcp_listeners = {
-    port     = 80
-    protocol = "HTTP"
-    target_group_index = 0
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
       }
     }
+  ]
 
-  target_group_arns = module.blog_alb.target_group_arns
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = "aws_instance.blog.id"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
 
 
 module "blog_sg" {
