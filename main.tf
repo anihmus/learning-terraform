8530e0910f70fa735908cb438bb9f44fb4df13a4
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-05 08:05:56 -0700

    Add vpc zone identifier

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "blog_alb" {
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
+  vpc_zone_identifier = mod.blog_vpc.public_subnets
   
   target_groups = [
     {
