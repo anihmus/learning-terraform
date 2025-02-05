2fd29c58f824fd0426e4cac19f3417bfddc71cc9
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-05 08:22:16 -0700

    define vpc in root module

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ data "aws_ami" "app_ami" {
 
 data "aws_vpc" "default" {
 default = true
+vpc_zone_identifier = mod.blog_vpc.public_subnets
 }
 
 module "autoscaling" {
@@ -42,7 +43,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  vpc_zone_identifier = mod.blog_vpc.public_subnets
   
   tags = {
     Name = "blog_vpc"
