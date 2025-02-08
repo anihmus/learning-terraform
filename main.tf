4e9921b909580b8634fc9271b6937bd5e2ba40a6
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-08 10:03:06 -0700

    remove braces

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = [mod.blog_vpc.public_subnets]
+  vpc_zone_identifier = mod.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
   image_id                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
