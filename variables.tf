e1cb7d3d4b58494705eccb3f5a50f8eb9b4a108e
Andrew Pomponio <andrew.pomponio@gmail.com>
2025-02-09 09:51:59 -0700

    Update variables

--- variables.tf
+++ variables.tf
@@ -13,14 +13,9 @@ variable "ami_filter" {
 
   default = {
     name   = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-    owners = ["979382823631"] # Bitnami
+    owner = ["979382823631"] # Bitnami
   }
 }
-
-data "aws_vpc" "default" {
-default = true
-}
-
 
 variable "environment" {
   description = "Development Environment"
@@ -29,18 +24,11 @@ variable "environment" {
     name           = string
     network_prefix = string
   })
-  name = "dev"
-  network_prefix = "10.0"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
   }
-}
 
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG"
